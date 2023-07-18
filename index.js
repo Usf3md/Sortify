7491f8449c23fc304c9abadef7a6bc49e6c5d8be
Youssef Emad <usf3md@gmail.com>
2023-07-18 12:50:12 +0300

    increased Gap time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector(".bars");
 let animationQueue = [];
 const bars = canvas.childNodes;
 let transitionTime;
-let timeGap = 100;
+let timeGap = 150;
 let canceled = false;
 let data = [1, 3, 5, 8, 7, 6, 4, 2];
 
